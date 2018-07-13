2a333a2bfeec8e8462244cffa257a75c2f6e9749
Vitor <vncoelho@gmail.com>
2018-07-13 15:46:07 -0300

    Cleaning

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Layout: Header Video",
-        "excerpt":"This post should display a header with a responsive video, if the theme supports it. Settings             Parameter      Required      Description                  id      Required      ID of the video              provider      Required      Hosting provider of the video, either youtube or vimeo      YouTube To embed the following YouTube video at url https://www.youtube.com/watch?v=XsxDH4HcOWA (long version) or https://youtu.be/XsxDH4HcOWA (short version) into a post or page’s main content you’d use: {% include video id=\"XsxDH4HcOWA\" provider=\"youtube\" %}  To embed it as a video header you’d use the following YAML Front Matter header:  video:    id: XsxDH4HcOWA    provider: youtubeVimeo To embed the following Vimeo video at url https://vimeo.com/212731897 into a post or page’s main content you’d use: {% include video id=\"212731897\" provider=\"vimeo\" %}  To embed it as a video header you’d use the following YAML Front Matter header:  video:    id: 212731897    provider: vimeo","categories": ["Layout","Uncategorized"],
-        "tags": ["video","layout"],
-        "url": "http://localhost:4000/layout/uncategorized/layout-header-video/",
-        "teaser":null},]
